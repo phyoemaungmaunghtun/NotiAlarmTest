f8fa4b4897130a31bb3a58d9b102e950690f655a
PhyoeMaung137 <phyoemaungmaung.htun@ascendcorp.com>
2022-03-20 17:42:19 +0630

    first commit

---
+++ app/src/main/java/com/example/notialarmtest/AlarmReceiver.kt
@@ -0,0 +1,59 @@
+package com.example.notialarmtest
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    /**
+     * sends notification when receives alarm
+     * and then reschedule the reminder again
+     * */
+    override fun onReceive(context: Context, intent: Intent) {
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.sendReminderNotification(
+            applicationContext = context,
+            channelId = context.getString(R.string.reminders_notification_channel_id)
+        )
+        // Remove this line if you don't want to reschedule the reminder
+        RemindersManager.startReminder(context.applicationContext,"17:30")
+    }
+}
+
+fun NotificationManager.sendReminderNotification(
+    applicationContext: Context,
+    channelId: String,
+) {
+    Log.d("Tag","Hello")
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        1,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    val builder = NotificationCompat.Builder(applicationContext, channelId)
+        .setContentTitle(applicationContext.getString(R.string.title_notification_reminder))
+        .setContentText(applicationContext.getString(R.string.description_notification_reminder))
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText(applicationContext.getString(R.string.description_notification_reminder))
+        )
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
+
+const val NOTIFICATION_ID = 1
---
+++ app/src/main/java/com/example/notialarmtest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.notialarmtest
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createNotificationsChannels()
+        RemindersManager.startReminder(this)
+    }
+
+    private fun createNotificationsChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d("Tag","Noti")
+            val channel = NotificationChannel(
+                getString(R.string.reminders_notification_channel_id),
+                getString(R.string.reminders_notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            ContextCompat.getSystemService(this, NotificationManager::class.java)
+                ?.createNotificationChannel(channel)
+        }
+    }
+}
